c57597a3e5cc02465f00bcc3dac782f36a48db3e
Adit <a.saxena.email@gmail.com>
2019-06-13 10:39:20 +0200

    test injection

--- src/containers/CampaignsPage.js
+++ src/containers/CampaignsPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 
-const Campaigns = inject('campaignsStore')(
-  observer(({ campaignsStore }) => {
+const Campaigns = inject('campaignsStore', 'auth')(
+  observer(({ campaignsStore, auth }) => {
     return (
       <div>
         {campaignsStore.campaigns.state === 'loading' && <div>Loading...</div>}
@@ -12,6 +12,7 @@ const Campaigns = inject('campaignsStore')(
           <ul>
             {campaignsStore.campaigns.list.map(campaign => <li key={campaign.id}>{campaign.name}</li>)}
           </ul>
+          <p>(injected auth, you are {auth.currentUser.name})</p>
         </div>}
       </div>
     );
